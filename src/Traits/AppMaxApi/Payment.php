39159a781bed81141495fa982c5701da5cc4f6b0
hdelima <hebert.dev@hotmail.com>
2021-05-18 10:49:56 -0300

    fix validation

--- src/Traits/AppMaxApi/Payment.php
+++ src/Traits/AppMaxApi/Payment.php
@@ -15,14 +15,19 @@ trait Payment
 		$this->validate( $data, [
 			'cart.order_id'								=> 'required|integer',
 			'customer.customer_id'						=> 'required|integer',
-			'payment.CreditCard.number'					=> 'required|size:16',
-			'payment.CreditCard.cvv'					=> 'required|between:2,4',
-			'payment.CreditCard.month'					=> 'required|integer',
-			'payment.CreditCard.year'					=> 'required|integer',
-			'payment.CreditCard.document_number'		=> 'required|size:11',
+			'payment.CreditCard'						=> 'required_without:payment.Boleto',
+			'payment.CreditCard.token'					=> 'nullable',
+			'payment.CreditCard.upsell_hash'			=> 'nullable',
+			'payment.CreditCard.number'					=> 'required_without:payment.CreditCard.token,payment.CreditCard.upsell_hash,payment.Boleto|size:16',
+			'payment.CreditCard.cvv'					=> 'required_without:payment.CreditCard.token,payment.CreditCard.upsell_hash,payment.Boleto|between:2,4',
+			'payment.CreditCard.month'					=> 'required_without:payment.CreditCard.token,payment.CreditCard.upsell_hash,payment.Boleto|integer',
+			'payment.CreditCard.year'					=> 'required_without:payment.CreditCard.token,payment.CreditCard.upsell_hash,payment.Boleto|integer',
+			'payment.CreditCard.document_number'		=> 'required_without:payment.Boleto|size:11',
 			'payment.CreditCard.name'					=> 'nullable|min:1',
-			'payment.CreditCard.installments'			=> 'required|integer',
+			'payment.CreditCard.installments'			=> 'required_without:payment.Boleto|required|integer',
 			'payment.CreditCard.soft_descriptor'		=> 'nullable|max:13',
+			'payment.Boleto'							=> 'required_without:payment.CreditCard',
+			'payment.Boleto.document_number'			=> 'required_without:payment.CreditCard|size:11'
 		]);
 
 		$this->options[ 'json' ] = $data;
