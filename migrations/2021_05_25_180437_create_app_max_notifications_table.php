2de315e408a4114836b56c3df3601a768f9b4138
hdelima <hebert.dev@hotmail.com>
2021-05-25 15:45:03 -0300

    created migration

---
+++ migrations/2021_05_25_180437_create_app_max_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppMaxNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_max_notifications', function (Blueprint $table) {
+            $table->id();
+			$table->string('environment')->nullable();
+			$table->string('event')->nullable();
+			$table->string('status')->nullable();
+			$table->json('data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_max_notifications');
+    }
+}
